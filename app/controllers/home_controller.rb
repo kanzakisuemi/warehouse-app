0f4e4c035c715ff300176c9c3d004bc2d92f6859
kanzakisuemi <jsuemikanzaki@gmail.com>
2024-04-08 16:03:15 -0300

    Usuário vê galpões na tela inicial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,28 @@
 class HomeController < ApplicationController
   def index
-    
+    @warehouses = Warehouse.all
+  end
+
+  def show
+    find_warehouse
+  end
+
+  def new
+  end
+
+  def create
+    warehouse = Warehouse.new(warehouse_params)
+    warehouse.save
+    redirect_to root_path
+  end
+
+  private
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :code, :city, :area)
+  end
+
+  def find_warehouse
+    @warehouse = Warehouse.find(params[:id])
   end
 end
